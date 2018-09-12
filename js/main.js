1af5739410e17c8ce33d6298a3fe59b414681378
Salami Usman Olawale <43177042+Walexitino@users.noreply.github.com>
2018-09-12 19:12:08 +0100

    portfolio

---
+++ js/main.js
@@ -0,0 +1,24 @@
+ //jQuery to collapse the navbar on scroll
+ $(window).scroll(function() {
+    if ($(".navbar-default").offset().top > 200) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } 
+    else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+  });
+
+  // Navigation scrolls
+  $('.navbar-nav li a').bind('click', function(event) {
+    $('.navbar-nav li').removeClass('active');
+    $(this).closest('li').addClass('active');
+    var $anchor = $(this);
+    var nav = $($anchor.attr('href'));
+    if (nav.length) {
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+
+      event.preventDefault();
+    }
+  });
